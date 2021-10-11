23fa8aeef248c4ab9909fa2fe02354b3a5ac2bf9
Valiev T.M <cptchunk@yandex.ru>
2021-10-11 22:36:18 +0500

    test_search_ya complete

--- test_search_ya.py
+++ test_search_ya.py
@@ -5,27 +5,44 @@ import time
 browser = webdriver.Chrome(
     executable_path="/home/imtoopunkforyou/prog/github/tensor-ru/chromedriver")
 url = "https://yandex.ru"
+keywrd = "Тензор"
 
-try:
-    browser.get("https://yandex.ru")
+def close_browser(): #for close the browser after testing
+    browser.close()
+    browser.quit()
+
+def test_search_input():
+    """Тест, проверяющий наличие поля поиска"""
+    browser.get(url)
+    browser.implicitly_wait(2)
+    search_input = browser.find_element_by_id("text")
+    assert search_input, f'На странице {url} нет поля для поиска'
+
+
+def test_suggest_window():
+    """Тест, проверяющий есть ли таблица подсказок"""
+    browser.get(url)
     search_input = browser.find_element_by_id("text")
     search_input.clear()
-    search_input.send_keys("Тензор")
-    time.sleep(2)
-    suggest_window = browser.find_elements_by_css_selector(
-        "ul.mini-suggest__popup-content")  # подсказка
-    search_input.send_keys(Keys.RETURN)
-    time.sleep(2)
-    b_tags_set = browser.find_elements_by_tag_name("b")
+    search_input.send_keys(keywrd)
+    browser.implicitly_wait(2)
+    suggest_window = browser.find_elements_by_css_selector("ul.mini-suggest__popup-content")
+    assert suggest_window, 'Таблица с подсказками не появилась'
+
+def test_search_result():
+    """"Тест, проверяющий, есть ли в первых 5 результатах поиска tensor.ru"""
     links = []
+    browser.get(url)
+    search_input = browser.find_element_by_id("text")
+    search_input.clear()
+    search_input.send_keys(keywrd)
+    search_input.send_keys(Keys.RETURN)
+    browser.implicitly_wait(2)
+    b_tags_set = browser.find_elements_by_tag_name("b")
     for link in b_tags_set:
         link = link.text
         if link.count("."):
             links.append(link)
-    print(links)
-    time.sleep(10000000)
-except Exception:
-    print(Exception)
-finally:
-    browser.close()
-    browser.quit()
+    assert links.index("tensor.ru") <= 4, 'В первых 5 результатах поиска отсутствует tensor.ru'
+    close_browser()
+
